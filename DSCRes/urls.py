8425f13d04975530004a4af133fd1ae89af76261
Pulkit Jatav <jatavpulkit@gmail.com>
2019-04-26 08:11:09 +0530

    added frontend to base html

--- DSCRes/urls.py
+++ DSCRes/urls.py
@@ -2,7 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from Resource import views
-from . import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -13,3 +14,6 @@ urlpatterns = [
     path('accounts/logout', views.logout_view, name="logout_view"),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
